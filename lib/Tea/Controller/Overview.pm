370d470cba087ee565adfb67fe8fa0ab8ec2b87a
noefp <noeisneo@gmail.com>
2016-04-07 11:51:00 -0400

    projects moved from overview to anatomy viewer

--- lib/Tea/Controller/Overview.pm
+++ lib/Tea/Controller/Overview.pm
@@ -25,25 +25,6 @@ Catalyst Controller.
 sub index :Path('/overview/') :Args(0) {
     my ( $self, $c ) = @_;
     
-    my $dbname = $c->config->{dbname};
-    my $host = $c->config->{dbhost};
-    my $username = $c->config->{dbuser};
-    my $password = $c->config->{dbpass};
-
-    my $schema = Tea::Schema->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
-    my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
-    
-    my @project_ids;
-    my %project_names;
-  
-    my $all_rs = $schema->resultset("Project");
-    while(my $n = $all_rs->next) {
-      push (@project_ids,$n->project_id);
-      $project_names{$n->project_id} = $n->name;
-    }
-    
-    $c->stash(project_ids => \@project_ids);
-    $c->stash(project_names => \%project_names);
     $c->stash(template => 'overview.mas');
 }
 

2033afd5a1a692eadf3ce1a40c38c2a2b9323a28
noefp <noeisneo@gmail.com>
2016-10-24 11:18:54 -0400

    anatomy viewer microscopy images

--- lib/Tea/Controller/Anatomy_viewer.pm
+++ lib/Tea/Controller/Anatomy_viewer.pm
@@ -21,11 +21,57 @@ Catalyst Controller.
 
 =cut
 
-sub index :Path('/anatomy_viewer/tomography/') :Args(0) {
+sub anatomy_viewer :Path('/anatomy_viewer/tomography/') :Args(0) {
     my ( $self, $c ) = @_;
-    $c->stash(template => 'anatomy_viewer.mas');
+    $c->stash(template => 'anatomy_viewer/anatomy_viewer.mas');
 }
 
+sub microscopy :Path('/anatomy_viewer/microscopy/') :Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash(template => 'anatomy_viewer/microscopy.mas');
+}
+
+sub slm82_pericarp :Path('/anatomy_viewer/microscopy/slm82_pericarp') :Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash(template => 'anatomy_viewer/slm82_pericarp.mas');
+}
+
+sub slm82_fruit :Path('/anatomy_viewer/microscopy/slm82_fruit') :Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash(template => 'anatomy_viewer/slm82_fruit.mas');
+}
+
+sub pimpi_fruit :Path('/anatomy_viewer/microscopy/pimpi_fruit') :Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash(template => 'anatomy_viewer/pimpi_fruit.mas');
+}
+
+
+sub av_menu :Path('/anatomy_viewer/') :Args(0) {
+    my ( $self, $c ) = @_;
+  
+    my $dbname = $c->config->{dbname};
+    my $host = $c->config->{dbhost};
+    my $username = $c->config->{dbuser};
+    my $password = $c->config->{dbpass};
+
+    my $schema = Tea::Schema->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
+    my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
+  
+    my @project_ids;
+    my %project_names;
+
+    my $all_rs = $schema->resultset("Project");
+    while(my $n = $all_rs->next) {
+      push (@project_ids,$n->project_id);
+      $project_names{$n->project_id} = $n->name;
+    }
+  
+    $c->stash(project_ids => \@project_ids);
+    $c->stash(project_names => \%project_names);
+    $c->stash(template => 'anatomy_viewer/anatomy_menu.mas');
+
+}
 
 
 =encoding utf8
--- lib/Tea/Controller/Tea_navigation.pm
+++ lib/Tea/Controller/Tea_navigation.pm
@@ -40,52 +40,6 @@ sub contact :Path('/contact/') :Args(0) {
 }
 
 
-sub microscopy :Path('/anatomy_viewer/microscopy/') :Args(0) {
-    my ( $self, $c ) = @_;
-    $c->stash(template => 'microscopy.mas');
-}
-
-sub slm82_pericarp :Path('/anatomy_viewer/microscopy/slm82_pericarp') :Args(0) {
-    my ( $self, $c ) = @_;
-    $c->stash(template => 'slm82_pericarp.mas');
-}
-
-sub slm82_fruit :Path('/anatomy_viewer/microscopy/slm82_fruit') :Args(0) {
-    my ( $self, $c ) = @_;
-    $c->stash(template => 'slm82_fruit.mas');
-}
-
-sub pimpi_fruit :Path('/anatomy_viewer/microscopy/pimpi_fruit') :Args(0) {
-    my ( $self, $c ) = @_;
-    $c->stash(template => 'pimpi_fruit.mas');
-}
-
-
-sub av_menu :Path('/anatomy_viewer/') :Args(0) {
-    my ( $self, $c ) = @_;
-  
-    my $dbname = $c->config->{dbname};
-    my $host = $c->config->{dbhost};
-    my $username = $c->config->{dbuser};
-    my $password = $c->config->{dbpass};
-
-    my $schema = Tea::Schema->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
-    my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
-  
-    my @project_ids;
-    my %project_names;
-
-    my $all_rs = $schema->resultset("Project");
-    while(my $n = $all_rs->next) {
-      push (@project_ids,$n->project_id);
-      $project_names{$n->project_id} = $n->name;
-    }
-  
-    $c->stash(project_ids => \@project_ids);
-    $c->stash(project_names => \%project_names);
-    $c->stash(template => 'anatomy_menu.mas');
-
-}
 
 
 =encoding utf8

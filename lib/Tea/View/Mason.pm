492db3eb5704becdf7f521b61c0a1f84b8fb855f
noefp <noeisneo@gmail.com>
2016-11-03 15:04:20 -0400

    mason ready to be overwriten

--- lib/Tea/View/Mason.pm
+++ lib/Tea/View/Mason.pm
@@ -1,3 +1,10 @@
+=head1 NAME
+Tea::View::Mason - Mason View Component for SGN
+=head1 DESCRIPTION
+Mason View Component for TEA. This extends Catalyst::View::HTML::Mason.
+=cut
+
+
 package Tea::View::Mason;
 
 use strict;
@@ -12,10 +19,10 @@ with 'Catalyst::Component::ApplicationAttribute';
 # use Catalyst qw/$c /;
 
 __PACKAGE__->config(
-    # globals => ['$c'],
+    globals => ['$c'],
     template_extension => '.mas',
     interp_args => {
-        #data_dir => Tea->tempfiles_base->subdir('mason'),
+        # data_dir => Tea->tempfiles_base->subdir('mason'),
         comp_root => [
             [ main => Tea->path_to('mason') ],
         ],
@@ -23,30 +30,51 @@ __PACKAGE__->config(
 );
 
 
-=he
+=head1 CONFIGURATION SETTINGS (which are also accessors)
 
+=head2 add_comp_root
 
-=head1 NAME
-
-Expr::View::Mason - Mason View Component for Expr
-
-=head1 DESCRIPTION
-
-Mason View Component for Expr
-
-=head1 SEE ALSO
-
-L<Expr>, L<HTML::Mason>
-
-=head1 AUTHOR
-
-noe,,,,
-
-=head1 LICENSE
-
-This library is free software . You can redistribute it and/or modify it under
-the same terms as perl itself.
+Configurable arrayref of additional Mason component roots.  These will
+be searched before the default ones.  Must be absolute paths.
 
 =cut
 
+# munges the interp_args comp_root to include the add_comp_root
+# configuration, plus the comp roots for SiteFeatures
+sub COMPONENT {
+    my ( $class, $c, $args ) = @_;
+
+    $args = $class->merge_config_hashes( $class->config, $args );
+
+    # coerce add_comp_root to arrayref
+    if( $args->{add_comp_root} && ! ref $args->{add_comp_root} ) {
+        $args->{add_comp_root} = [ $args->{add_comp_root} ];
+    }
+
+    # add comp roots for features and add_comp_root
+    unshift @{ $args->{interp_args}->{comp_root} }, (
+            # add_comp_root
+            ( map [ additional => $_ ], @{ $args->{add_comp_root} || [] } ),
+    );
+
+    return $class->new($c, $args);
+}
+
+=head1 FUNCTIONS
+
+=head2 $self->component_exists($component)
+
+Check if a Mason component exists. Returns 1 if the component exists, otherwise 0.
+
+=cut
+
+sub component_exists {
+    my ( $self, $component ) = @_;
+
+    return $self->interp->comp_exists( $component ) ? 1 : 0;
+}
+
+
+
+__PACKAGE__->meta->make_immutable;
 1;

5340db2993f0453f46d4a6548f1082a6505e8e6b
Mirella Flores <mrf252@cornell.edu>
2020-09-11 19:19:44 -0400

    read corr from indexed Lucy

--- lib/Tea/Controller/network_ajax.pm
+++ lib/Tea/Controller/network_ajax.pm
@@ -28,184 +28,132 @@ __PACKAGE__->config(
     map => { 'application/json' => 'JSON', 'text/html' => 'JSON' },
    );
 
-sub _get_correlated_genes_for_plot {
+sub _get_correlated_genes_edges {
     my $c = shift;
     my $corr_filter = shift;
     my $corr_index_path = shift;
     my $query_gene = shift;
 
-    my @genes;
+    my @genes; my @edges;
 
+    if ($corr_filter > 1) {
+    	$corr_filter = $corr_filter/100;
+    }
 
-
-	if ($corr_filter > 1) {
-		$corr_filter = $corr_filter/100;
-	}
-
-	my $lucy_corr = Lucy::Simple->new(
-	    path     => $corr_index_path,
-	    language => 'en',
-	);
-
-	my $sort_spec = Lucy::Search::SortSpec->new(
-	     rules => [
-		 	Lucy::Search::SortRule->new( field => 'correlation', reverse => 1,),
-		 	Lucy::Search::SortRule->new( field => 'gene2', reverse => 0,),
-		 	Lucy::Search::SortRule->new( field => 'gene1',),
-	     ],
-	);
-
-  my $hits;
-
-
-
-    $hits = $lucy_corr->search(
-      query      => $query_gene,
-      sort_spec  => $sort_spec,
-      num_wanted => 10000,
+    my $lucy_corr = Lucy::Simple->new(
+        path     => $corr_index_path,
+        language => 'en',
     );
 
-	while ( my $hit = $lucy_corr->next ) {
+    my $sort_spec = Lucy::Search::SortSpec->new(
+         rules => [
+    	 	Lucy::Search::SortRule->new( field => 'correlation', reverse => 1,),
+    	 	Lucy::Search::SortRule->new( field => 'gene2', reverse => 0,),
+    	 	Lucy::Search::SortRule->new( field => 'gene1',),
+         ],
+    );
 
-		if ($query_gene eq $hit->{gene1} && $hit->{correlation} >= $corr_filter) {
-			push(@genes, $hit->{gene2});
-		} elsif ($query_gene eq $hit->{gene2} && $hit->{correlation} >= $corr_filter) {
-			push(@genes, $hit->{gene1});
-		}
-    else {
+    my $hits = $lucy_corr->search(
+        query      => $query_gene,
+        sort_spec  => $sort_spec,
+        num_wanted => 10000,
+      );
 
+    my $id = 0;
+    my $gene_id = 0;
+    my $gene_id2 = 1;
+
+    push @genes, {
+              id => $gene_id,
+              name => $query_gene,
+              query => 'true'
+          };
+
+    while ( my $hit = $lucy_corr->next ) {
+
+        if ($query_gene eq $hit->{gene1} && $hit->{correlation} >= $corr_filter) {
+            push @genes, {
+                id => $gene_id2,
+                name => $hit->{gene2}
+            };
+            push @edges, {
+                id => $id,
+                source => $gene_id,
+                target => $gene_id2,
+                weight => $hit->{correlation} + 0,
+            };
+        } elsif ($query_gene eq $hit->{gene2} && $hit->{correlation} >= $corr_filter) {
+            push @genes, {
+                id => $gene_id2,
+                name => $gene_id2
+            };
+            push @edges, {
+                id => $id,
+                source => $gene_id,
+                target => $gene_id2,
+                weight => $hit->{correlation} + 0,
+            };
+        }
+        $id++; $gene_id2++;
     }
-	}
-    return (\@genes);
+
+    return (\@genes,\@edges);
 }
 
 sub _check_gene_exists {
     my $c = shift;
 
-  my $lucy_path = shift;
-  my $query_gene = shift;
+    my $lucy_path = shift;
+    my $query_gene = shift;
 
-  # test gene exist
-	my $lucy = Lucy::Simple->new(
-	    path     => $lucy_path,
-	    language => 'en',
-	);
+    # test gene exist
+    my $lucy = Lucy::Simple->new(
+        path     => $lucy_path,
+        language => 'en',
+    );
 
-  my $gene_found_num = $lucy->search(
-    query      => '22.65',
-  	num_wanted => 10
-  );
+    my $gene_found_num = $lucy->search(
+      query      => '22.65',
+    	num_wanted => 10
+    );
 
-	if (!$gene_found_num){
-		$c->stash->{errors} = "Gene not found";
-		$c->stash->{template} = '/Expression_viewer/output.mas';
-		return;
-	}
-print STDERR "Output: ".$gene_found_num."\n";
-return;
+    if (!$gene_found_num){
+    	$c->stash->{errors} = "Gene not found";
+    	$c->stash->{template} = '/Expression_viewer/output.mas';
+    	return;
+    }
+    print STDERR "Output: ".$gene_found_num."\n";
+    return;
 }
 
  sub get_network :Path('/expression_viewer/get_network/') :Args(0) {
-     my ( $self, $c ) = @_;
-#     my $corr_filter = $c->req->param("corr_filter_to_set_genes");
-#	 my $query_gene = $query_gene_array[0];
-#	 my @genes;
+    my ( $self, $c ) = @_;
+    my $schema = $self->schema;
+    my $project_id = $c->req->param("projectid");
+    my $corr_filter_value = $c->req->param("corrfiltervalue"); # '0.6';
+    my $query_gene = $c->req->param("inputgene"); 
+    my $project_rs = $schema->resultset('Project')->search({project_id => $project_id})->single;
+    my $corr_path = $c->config->{correlation_indexes_path};
+    my $corr_index_path = $corr_path."/".$project_rs->indexed_dir;
+print Dumper $query_gene;
+
+    my ($genes,$edges) = _get_correlated_genes_edges($c,$corr_filter_value,$corr_index_path,$query_gene);
+
+    my $message_back = "Returned ajax";
+
+    my $json_string = new JSON;
+    my $json_string2 = new JSON;
+
+    $json_string = encode_json($genes);
+    $json_string2 = encode_json($edges);
 
 
-     print STDERR "Made it inside ajax request\n";
-
-#     ($genes) = _get_correlated_genes_for_plot($c,$corr_filter,$corr_index_path,$query_gene);
-
-     my $message_back = "Returned ajax";
-	my @nodes = (
-    {
-       id => 0,
-       name  => "Solyc01g102660",
-#       name => "50",
-#       name => "70",
-    },
-    {
-       id => 1,
-       name => "Solyc03g095290",
-#       sample1_exp    => "150",
-#       sample2_exp     => "140",
-    },
-
-    {
-       id => 2,
-       name => "Solyc03g116570",
-#       sample1_exp    => "300",
-#       sample2_exp     => "330",
-    },
-
-    {
-       id => 3,
-       name  => "Solyc04g016470",
-    },
-
-  );
-
-     	my @edges = (
-    {
-       id => 0,
-       source  => 1,
-       target => 2,
-       weight => 0.35,
-    },
-    {
-       id => 1,
-       source  => 2,
-       target => 3,
-       weight => 0.1,
-    },
-
-    {
-       id => 2,
-       source  => 1,
-       target => 3,
-       weight => 0.9,
-    },
-
-    {
-       id => 3,
-       source  => 0,
-       target => 3,
-       weight => 0.4,
-    },
-
-    {
-       id => 4,
-       source  => 0,
-       target => 2,
-       weight => 0.6,
-    },
-
-    {
-       id => 5,
-       source  => 0,
-       target => 1,
-       weight => 0.2,
-    },
-
-  );
-
-     my $json_string;
-     my $json_string = new JSON;
-     $json_string = encode_json(\@nodes);
-     my $json_string2;
-     my $json_string2 = new JSON;
-     $json_string2 = encode_json(\@edges);
-
-#     print STDERR $json_string;
-#     $c->stash->{rest} = returnmessage => $json_string;
-#     my $json_string = 9;
     $c->stash->{rest} = {
-#	$c->stash->{rest} = expression_to_plot1 => $sample1_test;
-#	$c->stash->{rest} = expression_to_plot2 => $sample2_test;
-	node_genes_to_plot => $json_string,
-	edges_to_plot => $json_string2,
+
+        genes => $json_string,
+        edges => $json_string2,
 
     };
 
-     return;
+    return;
 }
--- root/static/js/network_tab.js
+++ root/static/js/network_tab.js
@@ -3,41 +3,47 @@ $(document).ready(function () {
     var network_loaded = 0;
 
     $("#network_tab").click(function(){
-    $("#dwl_expr_data").css("display","none");
+	    $("#dwl_expr_data").css("display","none");
 
-	d3.select("svg").remove();
+		d3.select("svg").remove();
 
-	d3.json("/static/data_genes.json", function(genes) {
-		// console.log(data);
+		var genes;
+		var edges;
+
+		jQuery.ajax({
+		    url: '/expression_viewer/get_network/',
+ 		    async: false,
+		    method: 'GET',
+		    dataType: "json",
+		    data: { 'projectid': project_id, 'corrfiltervalue' : 0.7, 'inputgene' : gene_id } ,
+		    success: function(response) {
+				genes = JSON.parse(response.genes);
+				edges = JSON.parse(response.edges);
+ 		    },
+ 		    error: function(response) {
+                alert(response);
+           }
+		});
 		
-		d3.json("/static/data_edges.json", function(edges) {
-
-		//if (!network_loaded) {
-			makenetwork(genes,edges);
-		//}
-		return edges;
-	});
-		return genes;
-	});
-
-
-	function makenetwork(genes_in,edges_in){
-
-		var network = d3.network()
-		            .genes(genes_in)
-		            .edges(edges_in)
-		            .geneText(function(d) { return d.name; });
-
-		d3.select("#container_network")
-		.append("svg")
-		.attr("width", 1000)
-		.attr("height", 1000)
-		.call(network);
-
-		network.showLegend().filter(.08, 60).draw();
-		} //end of makenetwork function
-
+		makenetwork(genes,edges);
 		network_loaded = 1;
 
 	});
+
+	function makenetwork(genes_in,edges_in){
+
+			var network = d3.network()
+			            .genes(genes_in)
+			            .edges(edges_in)
+			            .geneText(function(d) { return d.name; });
+
+			d3.select("#container_network")
+			.append("svg")
+			.attr("width", 1000)
+			.attr("height", 1000)
+			.call(network);
+
+			// network.showLegend().filter(.1, 5).draw();
+			network.showLegend().draw();
+		} //end of makenetwork function
 });

f6d86514bcdf2e3c5d7eb4ed0be92c0cd324dcdb
noefp <noeisneo@gmail.com>
2020-11-03 14:40:24 +0100

    data sets order by species

--- lib/Tea/Controller/Tea_navigation.pm
+++ lib/Tea/Controller/Tea_navigation.pm
@@ -60,26 +60,25 @@ sub project_menu :Path('/expression_viewer/project_menu/') :Args(0) {
     my $schema = Tea::Schema->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
     my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;", "$username", "$password");
 
-    my @project_ids;
-    my %project_names;
+    my @project_html;
 
     my $project_ordinal;
     my %project_name_hash;
     my %project_order_hash;
+    my %project_sps;
+    my %sps_name_hash;
+
+    my $sps_rs = $schema->resultset("Organism");
+    while(my $sps_obj = $sps_rs->next) {
+      $sps_name_hash{$sps_obj->organism_id} = $sps_obj->species;
+    }
 
     my $all_rs = $schema->resultset("Project");
 
-
-    # while(my $n = $all_rs->next) {
-      # push (@project_ids,$n->project_id);
-      # $project_names{$n->project_id} = $n->name;
-    # }
-
-
     while(my $proj_obj = $all_rs->next) {
-      $project_names{$proj_obj->project_id} = $proj_obj->name;
       my $project_name = $proj_obj->name;
       my $project_id = $proj_obj->project_id;
+      my $sps_id = $proj_obj->organism_id;
 
       if ($proj_obj->ordinal) {
         $project_ordinal = $proj_obj->ordinal;
@@ -88,18 +87,30 @@ sub project_menu :Path('/expression_viewer/project_menu/') :Args(0) {
         $project_ordinal = $project_id;
       }
 
+      $project_sps{$project_id} = $sps_id;
       $project_name_hash{$project_id} = $project_name;
       $project_order_hash{$project_ordinal} = $project_id;
     }
 
-    foreach my $key (sort {$a <=> $b} keys %project_order_hash) {
-      my $project_id = $project_order_hash{$key};
-      push (@project_ids,$project_id);
+
+    foreach my $sps_id (sort { $sps_name_hash{$a} cmp $sps_name_hash{$b} } keys %sps_name_hash) {
+      # print STDERR $sps_name_hash{$sps_id}."\n";
+      push (@project_html, "<h3><i>$sps_name_hash{$sps_id}</i></h3><ul id=\"dataset_list\">");
+
+
+      foreach my $key (sort {$a <=> $b} keys %project_order_hash) {
+        my $project_id = $project_order_hash{$key};
+
+        if ($sps_id == $project_sps{$project_id}) {
+          # print STDERR $project_name_hash{$project_id}."\n";
+          push (@project_html, "<li><a href=\"project_page?project_id=$project_id\" class=\"font24\">$project_name_hash{$project_id}</a></li>");
+        }
+      }
+      push (@project_html, "</ul>");
+
     }
 
-
-    $c->stash(project_ids => \@project_ids);
-    $c->stash(project_names => \%project_names);
+    $c->stash(project_html => \@project_html);
     $c->stash(template => 'project_menu.mas');
 }
 
